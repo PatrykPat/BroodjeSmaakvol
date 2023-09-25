755b633e3cf7a56ecc9df31fc1cb8cff0394bbb9
PatrykPerwenis <102594009+PatrykPerwenis@users.noreply.github.com>
2023-09-25 10:08:59 +0200

    bezig met login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,49 @@
 <x-guest-layout>
+    <style>
+    .inputveld{
+        background-color: #F1F2F2;
+        color: black;
+        border-radius: 23px;
+    }
+    .loginbutton{
+        background-color: #EB5258;
+        border-radius: 23px;
+    }
+    
+    </style>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <x-auth-session-status class="inputveld" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-label for="email" :value="__('Login')" />
+            <x-text-input id="email" class="inputveld mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="School ID"/>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- <x-input-label for="password" :value="__('Password')" /> -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="inputveld mt-1 w-full"
                             type="password"
                             name="password"
+                            placeholder="Wachtwoord"
                             required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <!-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> -->
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
@@ -39,7 +52,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
+            <x-primary-button class="ml-3 loginbutton">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
