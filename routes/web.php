10fc74934fef94fe62e07c0df484822df3767b06
kwimpie <105362709+sidv46@users.noreply.github.com>
2023-10-03 10:39:42 +0200

    meldingen pagina

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/meldingen', function () {
+    return view('melding');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
